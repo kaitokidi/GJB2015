36566c673e98c2a79463c8a321ccea0bf6b2fb38
Obscurs <ferris202@gmail.com>
2015-01-23 22:45:13 +0100

    un poquito de sidita

--- Player.cpp
+++ Player.cpp
@@ -3,7 +3,7 @@
 
 int SPRITE_FOOT_X = 10;
 int SPRITE_FOOT_Y = 10;
-Player::Player() :Collisionable(Resources::playerTexture, SPRITE_FOOT_X, SPRITE_FOOT_Y, 1, 1){
+Player::Player() :Collisionable(Resources::playerTexture, PLAYER_0_SIZE_X, PLAYER_0_SIZE_Y, 1, 1){
     //sprite.setTexture(Resources::playerTexture);
 }
 
