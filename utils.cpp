e89e54ebda994f9b75f2b5ee02df9de8c2c1f0cf
Pinkii- <pinkiibiza@gmail.com>
2015-01-23 22:57:26 +0100

    ienis rompitio nomas la clase masno

--- utils.cpp
+++ utils.cpp
@@ -1,6 +1,6 @@
 #include "utils.hpp"
 
-direction { down, left, right, up , none };
+//direction { down, left, right, up , none };
 const int mx[5] = {0,  -1,	 1,	   0,   0   };
 const int my[5] = {1,	0,	 0,   -1,   0   };
 
