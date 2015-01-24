a5d15713f15aa051fd941d679bfe9ad5a548a178
Obscurs <ferris202@gmail.com>
2015-01-24 04:33:53 +0100

    timer utils

--- utils.cpp
+++ utils.cpp
@@ -9,6 +9,6 @@ const int PLAYER_MAX_SPEED[7] =    {100,   0,   0,   0,   0,   0,   0};
 const int PLAYER_ACCELERATION[7] = {100,   0,   0,   0,   0,   0,   0};
 const int PLAYER_SIZE_X[7] =       { 10,  10,  10,  10,  10,  10,  10};
 const int PLAYER_SIZE_Y[7] =       { 10,  10,  10,  10,  10,  10,  10};
-const int PLAYER_ANIMATION_TIMER[7] =       { 10,  10,  10,  10,  10,  10,  10};
+const int PLAYER_ANIMATION_TIMER[7] =       { 100,  100,  100,  100,  100,  100,  100};
 
 const float GRAVITY = 98;
