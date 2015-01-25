e4be89433341cb76e894a0dc4037f84a1d288738
Pinkii <pinkiibiza@gmail.com>
2015-01-25 01:02:28 +0100

    ajoracompilanomas

--- BodyPart.cpp
+++ BodyPart.cpp
@@ -8,25 +8,25 @@ BodyPart::BodyPart(GameManager *gm, float px, float py, float sx, float sy, int 
     
         switch (idBody){
             case 0:
-                sprite.setTexture(&Resources::shoesText);
+                sprite.setTexture(Resources::shoesText);
                 break;
             case 1:
-                sprite.setTexture(&Resources::legsText);
+                sprite.setTexture(Resources::legsText);
                 break;
             case 2:
-                sprite.setTexture(&Resources::bodyText);
+                sprite.setTexture(Resources::bodyText);
                 break;
             case 3:
-                sprite.setTexture(&Resources::armsText);
+                sprite.setTexture(Resources::armsText);
                 break;
             case 4:
-                sprite.setTexture(&Resources::handText);
+                sprite.setTexture(Resources::handText);
                 break;
             case 5:
-                sprite.setTexture(&Resources::headText);               
+                sprite.setTexture(Resources::headText);
                 break;
             case 6:
-                sprite.setTexture(&Resources::wingsText);
+                sprite.setTexture(Resources::wingsText);
                 break;
             default:
                 break;
